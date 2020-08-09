7dc59384101e2a9ea7cfcbf20d1a48c421a88f85
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2020-08-09 18:59:21 +0200

    Implement UDP command functionality

--- include/libmueb/muebcontroller.h
+++ include/libmueb/muebcontroller.h
@@ -1,6 +1,7 @@
 #ifndef MUEBCONTROLLER_H
 #define MUEBCONTROLLER_H
 
+#include <QHostAddress>
 #include <QObject>
 #include <memory>
 
@@ -14,15 +15,39 @@ class LIBMUEB_EXPORT MuebController final : public QObject {
   Q_DISABLE_COPY(MuebController)
 
  public:
+  enum Commands {
+    turn_12v_off_left = 0x00,       //!< Turn left panel's 12v off
+    turn_12v_off_right = 0x01,      //!< Turn right panel's 12v off
+    reset_left_panel = 0x02,        //!< Reset left panel state
+    reset_right_panel = 0x03,       //!< Reset right panel state
+    reboot = 0x04,                  //!< Reboot device
+    get_status = 0x05,              //!< Get device's status
+    get_mac = 0x07,                 //!< Get device's MAC address
+    use_internal_anim = 0x10,       //!< Use internal animation
+    use_external_anim = 0x20,       //!< Use external animation
+    blank = 0x30,                   //!< Blank both panels
+    delete_network_buffers = 0x06,  //!< Flush network buffers
+    ping = 0x40,                    //!< Send back 'pong' response
+    start_firmware_update = 0x50,   //!< Start firmware update process
+    get_firmware_checksum = 0x51,   //!< Return main program checksum
+    swap_windows = 0x70,            //!< Swap left and right windows
+    set_whitebalance = 0x80         //!< Set white balance
+  };
+
   static MuebController& getInstance();
 
+  void sendCommand(Commands command, QHostAddress target, bool broadcastCommand,
+                   QByteArray macAddress = "");
+
  signals:
+  void commandResponse(QString val);
 
  private:
   std::unique_ptr<MuebControllerPrivate> const d_ptr;
 
   MuebController();
   ~MuebController();
+  void readPendingDatagrams();
 };
 
 #endif  // MUEBCONTROLLER_H
--- src/muebcontroller.cpp
+++ src/muebcontroller.cpp
@@ -1,5 +1,9 @@
 #include "muebcontroller.h"
 
+#include <QDebug>
+#include <QNetworkDatagram>
+#include <QUdpSocket>
+
 class MuebControllerPrivate {
   Q_DISABLE_COPY(MuebControllerPrivate)
   Q_DECLARE_PUBLIC(MuebController)
@@ -10,7 +14,14 @@ class MuebControllerPrivate {
   explicit MuebControllerPrivate(MuebController* controller)
       : q_ptr(controller) {
     Q_Q(MuebController);
+
+    udpSocket.bind();
+
+    QObject::connect(&udpSocket, &QUdpSocket::readyRead, q,
+                     &MuebController::readPendingDatagrams);
   }
+
+  QUdpSocket udpSocket;
 };
 
 MuebController& MuebController::getInstance() {
@@ -18,7 +29,33 @@ MuebController& MuebController::getInstance() {
   return controller;
 }
 
+void MuebController::sendCommand(MuebController::Commands command,
+                                 QHostAddress target, bool broadcastCommand,
+                                 QByteArray macAddress) {
+  using namespace libmueb::defaults;
+  Q_D(MuebController);
+
+  QByteArray packet;
+  packet.append(commandMagic).append(command);
+
+  if (broadcastCommand) {
+    target = QHostAddress(broadcastAddress);
+    packet.append(1);
+    packet.append(macAddress);
+  }
+
+  d->udpSocket.writeDatagram(packet, target, commandPort);
+}
+
 MuebController::MuebController()
     : d_ptr(std::make_unique<MuebControllerPrivate>(this)) {}
 
 MuebController::~MuebController() = default;
+
+void MuebController::readPendingDatagrams() {
+  Q_D(MuebController);
+
+  while (d->udpSocket.hasPendingDatagrams()) {
+    emit commandResponse(d->udpSocket.receiveDatagram().data());
+  }
+}
